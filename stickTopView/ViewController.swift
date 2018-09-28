c29437e0242820271d809e564ff51f6d5569a85e
affixusimac1 <viveking@outlook.com>
2018-09-28 10:20:47 +0530

    Initial Commit

---
+++ stickTopView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stickTopView
+//
+//  Created by AFFIXUS IMAC1 on 9/28/18.
+//  Copyright © 2018 AFFIXUS IMAC1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
